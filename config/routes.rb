632b54872a53515b7bec19c4715784cc24b385a4
kan nanako <n.k.0213.0529@gmail.com>
2020-07-09 15:57:40 +0900

    ルートへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'blogs#index'
   resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
